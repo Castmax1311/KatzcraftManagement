fa40ebc4b22a8eb913c879924d3e002e0af5918a
Castmax1311 <109237939+Castmax1311@users.noreply.github.com>
2023-07-19 21:41:21 +0200

    v.0.6 | What kind of items do you have? & Bugfixes
    With this update you can use "/inventory <player>" as a server operator to look into the inventory of other players

--- src/main/java/de/castmax1311/katzcraftmanagement/Listeners/PlayerJoinListener.java
+++ src/main/java/de/castmax1311/katzcraftmanagement/Listeners/PlayerJoinListener.java
@@ -2,6 +2,9 @@ package de.castmax1311.katzcraftmanagement.Listeners;
 
 import de.castmax1311.katzcraftmanagement.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,6 +15,14 @@ public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+
+        Player player = event.getPlayer();
+        if (Main.instance.isMaintenanceMode() && !player.isOp()) {
+            player.kickPlayer(ChatColor.RED + "This server is under maintenance");
+            return;
+        } else {
+            player.setGameMode(GameMode.CREATIVE);
+        }
 
         if(Main.instance.getConfig().getString("customJoinMessage") != null) {
 
